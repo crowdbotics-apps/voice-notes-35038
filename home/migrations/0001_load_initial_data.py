98d78df3cbc389ed97845510b55c9bd0a1aed271
Crowdbotics <team@crowdbotics.com>
2022-05-02 02:18:48 +0000

    Initial commit.

---
+++ home/migrations/0001_load_initial_data.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def create_site(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    custom_domain = "voice-notes-35038.botics.co"
+
+    site_params = {
+        "name": "Voice notes",
+    }
+    if custom_domain:
+        site_params["domain"] = custom_domain
+
+    Site.objects.update_or_create(defaults=site_params, id=1)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("sites", "0002_alter_domain_unique"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_site),
+    ]
